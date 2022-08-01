6a5625697e16619da7d3202d490ef3a102b10c8d
Sandro <sandromarcosgouveia@gmail.com>
2022-08-01 18:25:29 -0300

    Tentando...

--- Main_lateral.py
+++ Main_lateral.py
@@ -33,38 +33,38 @@ def About(conteiner):
         st.text('In order to understand how these indicators works, please check the following link:\n'
                 'https://www.investopedia.com/ask/answers/122414/what-are-most-common-periods-used-creating-moving-average-ma-lines.asp')
 
-def LoadDdata(conteiner):
-    with conteiner:
-        st.title('Load your Stock Data')
-        st.text('Define your Stock')
-
-        today = datetime.date.today()
-        tomorrow = today - datetime.timedelta(days=500)
-        start_date = st.date_input('Start date', tomorrow)
-        end_date = st.date_input('End date', today)
-
-
-        stock = st.selectbox('Select the Stock to study', ('AAPL', 'GOOG', 'AMZN', 'MSFT'))
-
-        st.title('Plot your Stock')
-        OHLC = st.selectbox('Select Open, High, Low or Close', ('Open', 'High', 'Low', 'Close'))
-        ques = st.radio("Do you table or graph?", ('Graph', 'Table'))
-
-        data = get_data(stock, start_date, end_date)
-        if ques == 'Table':
-            st.dataframe(data)
-            dados = data[OHLC]
-        else:
-            dados = data[OHLC]
-            # st.line_chart(dados)
-            fig = plt.figure(figsize=[10, 5])
-            ax = fig.add_subplot()
-            ax.grid()
-            ax.plot(dados, c='b', lw=1, label=OHLC)
-            ax.legend(loc='best')
-            ax.tick_params(axis='x', labelrotation=45)
-            fig.tight_layout()
-            st.pyplot(fig=fig)
+# def LoadDdata(conteiner):
+#     with conteiner:
+#         st.title('Load your Stock Data')
+#         st.text('Define your Stock')
+#
+#         today = datetime.date.today()
+#         tomorrow = today - datetime.timedelta(days=500)
+#         start_date = st.date_input('Start date', tomorrow)
+#         end_date = st.date_input('End date', today)
+#
+#
+#         stock = st.selectbox('Select the Stock to study', ('AAPL', 'GOOG', 'AMZN', 'MSFT'))
+#
+#         st.title('Plot your Stock')
+#         OHLC = st.selectbox('Select Open, High, Low or Close', ('Open', 'High', 'Low', 'Close'))
+#         ques = st.radio("Do you table or graph?", ('Graph', 'Table'))
+#
+#         data = get_data(stock, start_date, end_date)
+#         if ques == 'Table':
+#             st.dataframe(data)
+#             dados = data[OHLC]
+#         else:
+#             dados = data[OHLC]
+#             # st.line_chart(dados)
+#             fig = plt.figure(figsize=[10, 5])
+#             ax = fig.add_subplot()
+#             ax.grid()
+#             ax.plot(dados, c='b', lw=1, label=OHLC)
+#             ax.legend(loc='best')
+#             ax.tick_params(axis='x', labelrotation=45)
+#             fig.tight_layout()
+#             st.pyplot(fig=fig)
 
 def PlotData(conteiner, data, OHLC):
     with conteiner:
@@ -82,30 +82,34 @@ def PlotData(conteiner, data, OHLC):
         fig.tight_layout()
         st.pyplot(fig=fig)
 
-        st.title('EDA')
-        st.text('Exploratory Data Analysis - Must be improved')
-
-        st.text('First discrete difference')
-        diff_o = data['Open'].diff(1)
-        diff_h = data['High'].diff(1)
-        diff_l = data['Low'].diff(1)
-        diff_c = data['Close'].diff(1)
-
-        fig = plt.figure(figsize=[10, 5])
-        ax1 = fig.add_subplot(2, 2, 1)
-        ax1.set_title('Open')
-        ax1.hist(diff_o, 30, density=1, color='green', alpha=0.7, ec='k')
-        ax1.set_title('High')
-        ax1 = fig.add_subplot(2, 2, 2)
-        ax1.hist(diff_h, 30, density=1, color='green', alpha=0.7, ec='k')
-        ax1.set_title('Low')
-        ax1 = fig.add_subplot(2, 2, 3)
-        ax1.hist(diff_l, 30, density=1, color='green', alpha=0.7, ec='k')
-        ax1.set_title('Close')
-        ax1 = fig.add_subplot(2, 2, 4)
-        ax1.hist(diff_c, 30, density=1, color='green', alpha=0.7, ec='k')
-        fig.tight_layout()
-        st.pyplot(fig=fig)
+        # st.title('EDA')
+        # st.text('Exploratory Data Analysis - Must be improved')
+        #
+        # st.text('First discrete difference')
+        # diff_o = data['Open'].diff(1)
+        # diff_h = data['High'].diff(1)
+        # diff_l = data['Low'].diff(1)
+        # diff_c = data['Close'].diff(1)
+        #
+        # fig = plt.figure(figsize=[10, 5])
+        # ax1 = fig.add_subplot(2, 2, 1)
+        # ax1.set_title('Open')
+        # ax1.hist(diff_o, 30, density=1, color='green', alpha=0.7, ec='k')
+        #
+        # ax1 = fig.add_subplot(2, 2, 2)
+        # ax1.set_title('High')
+        # ax1.hist(diff_h, 30, density=1, color='green', alpha=0.7, ec='k')
+        #
+        # ax1 = fig.add_subplot(2, 2, 3)
+        # ax1.set_title('Low')
+        # ax1.hist(diff_l, 30, density=1, color='green', alpha=0.7, ec='k')
+        #
+        # ax1 = fig.add_subplot(2, 2, 4)
+        # ax1.set_title('Close')
+        # ax1.hist(diff_c, 30, density=1, color='green', alpha=0.7, ec='k')
+        #
+        # fig.tight_layout()
+        # st.pyplot(fig=fig)
 
         # fig = plt.figure(figsize=[8, 2])
         # ax = fig.add_subplot()
@@ -141,71 +145,71 @@ with st.sidebar:
     data = get_data(stock, start_date, end_date)
 
 
-    st.title('Define your Indicator')
-    indicador1 = st.selectbox('Select the Indicator #1', ('SMA', 'EMA', 'DEMA'))
-    per1 = st.slider("Please enter periodo ind#1", 2, 150, 1)
-    indicador2 = st.selectbox('Select the Indicator #2', ('SMA', 'EMA', 'DEMA'))
-    per2 = st.slider("Please enter periodo ind#2", 2, 150, 1)
+    # st.title('Define your Indicator')
+    # indicador1 = st.selectbox('Select the Indicator #1', ('SMA', 'EMA', 'DEMA'))
+    # per1 = st.slider("Please enter periodo ind#1", 2, 150, 1)
+    # indicador2 = st.selectbox('Select the Indicator #2', ('SMA', 'EMA', 'DEMA'))
+    # per2 = st.slider("Please enter periodo ind#2", 2, 150, 1)
 
-    st.title('Forecast')
-    n_ahead = st.slider("Please, select the days ahead", 2, 500, 1)
-    ahead = today + datetime.timedelta(days=n_ahead-1)
-    data_afrente = pd.date_range(today, ahead, freq='d')
+    # st.title('Forecast')
+    # n_ahead = st.slider("Please, select the days ahead", 2, 500, 1)
+    # ahead = today + datetime.timedelta(days=n_ahead-1)
+    # data_afrente = pd.date_range(today, ahead, freq='d')
 
-with ctAppStra:
-    PlotData(ctPlot, data, OHLC)
-    st.title('Test your Strategy')
-    st.text('The strategy: When the indicator#1 is above the indicator#2,\nit is an entry sign represented by the green triangule(^)')
-    data = data
-
-    if indicador1 == 'SMA':
-        data_ind1 = TA.SMA(data, per1)
-    elif indicador1 == 'EMA':
-        data_ind1 = TA.EMA(data, per1)
-    else:
-        data_ind1 = TA.DEMA(data, per1)
-
-    if indicador1 == 'SMA':
-        data_ind2 = TA.SMA(data, per2)
-    elif indicador2 == 'EMA':
-        data_ind2 = TA.EMA(data, per2)
-    else:
-        data_ind2 = TA.DEMA(data, per2)
-
-    idx_ent, entrada = fun_strategy1(data[OHLC], data_ind1, data_ind2)
-
-    fig = plt.figure(figsize=[8, 3])
-    ax = fig.add_subplot()
-    ax.grid()
-    ax.plot(data[OHLC], c='b', lw=1, label='data')#===================================================================
-    ax.scatter(idx_ent, entrada, marker='^', c='g', label='entry')
-    ax.plot(data_ind1, ls='-', c='r', lw=0.75, label='Ind#1')#========================================================
-    ax.plot(data_ind2, ls='-', c='g', lw=0.75, label='Ind#2')#========================================================
-    ax.tick_params(axis='x', labelrotation=45)
-    ax.legend(loc='best')
-    fig.tight_layout()
-    st.pyplot(fig=fig)
-
-
-    # st.title('Time-Series Forecast')
-    # st.text('Forecast your stock using ARIMA approach - Must be improved')
-    # treino = data[OHLC]
-    # modl = pm.auto_arima(treino, start_p=1, start_q=1, start_P=1, start_Q=1,
-    #                      max_p=5, max_q=10, max_P=10, max_Q=10, seasonal=True,
-    #                      stepwise=True, suppress_warnings=True, D=20, max_D=20,
-    #                      error_action='ignore')
-    #
-    # preds, conf_int = modl.predict(n_periods=n_ahead, return_conf_int=True)
-    #
-    # fig = plt.figure(figsize=[8, 2])
-    # ax = fig.add_subplot()
-    # ax.grid()
-    # ax.plot(treino, alpha=0.75, c='b', label='Real')
-    # ax.plot(data_afrente, preds, alpha=0.75, c='r', label='Predict')
-    # plt.fill_between(data_afrente, conf_int[:, 0], conf_int[:, 1], alpha=0.1, color='b')
-    # ax.legend(loc='best')
-    # fig.tight_layout()
-    # st.pyplot(fig=fig)
+# with ctAppStra:
+#     PlotData(ctPlot, data, OHLC)
+#     st.title('Test your Strategy')
+#     st.text('The strategy: When the indicator#1 is above the indicator#2,\nit is an entry sign represented by the green triangule(^)')
+#     data = data
+#
+#     if indicador1 == 'SMA':
+#         data_ind1 = TA.SMA(data, per1)
+#     elif indicador1 == 'EMA':
+#         data_ind1 = TA.EMA(data, per1)
+#     else:
+#         data_ind1 = TA.DEMA(data, per1)
+#
+#     if indicador1 == 'SMA':
+#         data_ind2 = TA.SMA(data, per2)
+#     elif indicador2 == 'EMA':
+#         data_ind2 = TA.EMA(data, per2)
+#     else:
+#         data_ind2 = TA.DEMA(data, per2)
+#
+#     idx_ent, entrada = fun_strategy1(data[OHLC], data_ind1, data_ind2)
+#
+#     # fig = plt.figure(figsize=[8, 3])
+#     # ax = fig.add_subplot()
+#     # ax.grid()
+#     # ax.plot(data[OHLC], c='b', lw=1, label='data')#===================================================================
+#     # ax.scatter(idx_ent, entrada, marker='^', c='g', label='entry')
+#     # ax.plot(data_ind1, ls='-', c='r', lw=0.75, label='Ind#1')#========================================================
+#     # ax.plot(data_ind2, ls='-', c='g', lw=0.75, label='Ind#2')#========================================================
+#     # ax.tick_params(axis='x', labelrotation=45)
+#     # ax.legend(loc='best')
+#     # fig.tight_layout()
+#     # st.pyplot(fig=fig)
+#
+#
+#     # st.title('Time-Series Forecast')
+#     # st.text('Forecast your stock using ARIMA approach - Must be improved')
+#     # treino = data[OHLC]
+#     # modl = pm.auto_arima(treino, start_p=1, start_q=1, start_P=1, start_Q=1,
+#     #                      max_p=5, max_q=10, max_P=10, max_Q=10, seasonal=True,
+#     #                      stepwise=True, suppress_warnings=True, D=20, max_D=20,
+#     #                      error_action='ignore')
+#     #
+#     # preds, conf_int = modl.predict(n_periods=n_ahead, return_conf_int=True)
+#     #
+#     # fig = plt.figure(figsize=[8, 2])
+#     # ax = fig.add_subplot()
+#     # ax.grid()
+#     # ax.plot(treino, alpha=0.75, c='b', label='Real')
+#     # ax.plot(data_afrente, preds, alpha=0.75, c='r', label='Predict')
+#     # plt.fill_between(data_afrente, conf_int[:, 0], conf_int[:, 1], alpha=0.1, color='b')
+#     # ax.legend(loc='best')
+#     # fig.tight_layout()
+#     # st.pyplot(fig=fig)
 
 
 
